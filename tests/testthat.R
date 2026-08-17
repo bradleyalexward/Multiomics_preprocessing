907467c335832a22136309d7b8ffa4579dc57b60
Bradley Ward <bradleyalexward@gmail.com>
2026-08-17 23:14:04 +0200

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(Multiomics.Preprocessing)
+library(OmicsRefinery)
 
-test_check("Multiomics.Preprocessing")
+test_check("OmicsRefinery")
